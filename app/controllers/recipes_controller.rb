79299448d5f72c8fd95a90768e8791773a207af4
Acelya Uslu <ace.uslu@gmail.com>
2022-06-29 15:39:32 +0200

    added new & create actions with associated view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,4 +8,27 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
     authorize @recipe
   end
+
+  def new
+    @recipe = Recipe.new
+    authorize @recipe
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    # @recipe.user = current_user
+    authorize @recipe
+
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :duration, :description, :category_id, :level_id, :serving)
+  end
 end
